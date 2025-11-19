14c9be4ed225f135b9a3f26e7a7e0c3b1ae9afca
Luc Gaitskell <me@lucg.xyz>
2025-11-19 01:14:09 -0500

    Add my AXIS tb helper from class

---
+++ sim/bus/axis.py
@@ -0,0 +1,173 @@
+import os
+from cocotb.triggers import (
+    RisingEdge,
+    FallingEdge,
+    ReadOnly,
+    ReadWrite,
+)
+from cocotb_bus.drivers import BusDriver
+from cocotb_bus.monitors import BusMonitor
+
+test_file = os.path.basename(__file__).replace(".py", "")
+
+
+class AXIS_Monitor(BusMonitor):
+    """
+    monitors axi streaming bus
+    """
+
+    transactions = 0  # use this variable to track good ready/valid handshakes
+
+    def __init__(self, dut, name, clk, callback=None):
+        self._signals = [
+            "axis_tvalid",
+            "axis_tready",
+            "axis_tlast",
+            "axis_tdata",
+            "axis_tstrb",
+        ]
+        BusMonitor.__init__(self, dut, name, clk, callback=callback)
+        self.clock = clk
+        self.transactions = 0
+        self.dut = dut
+
+    async def _monitor_recv(self):
+        """
+        Monitor receiver
+        """
+        rising_edge = RisingEdge(self.clock)  # make these coroutines once and reuse
+        falling_edge = FallingEdge(self.clock)
+        read_only = ReadOnly()  # This is
+        while True:
+            # await rising_edge #can either wait for just edge...
+            # or you can also wait for falling edge/read_only (see note in lab)
+            await falling_edge  # sometimes see in AXI shit
+            await read_only  # readonly (the postline)
+            valid = self.bus.axis_tvalid.value
+            ready = self.bus.axis_tready.value
+            last = self.bus.axis_tlast.value
+            data = self.bus.axis_tdata.value  # .signed_integer
+            if valid and ready:
+                self.transactions += 1
+                thing = dict(
+                    data=data.signed_integer,
+                    last=last,
+                    name=self.name,
+                    count=self.transactions,
+                )
+                self.dut._log.info(f"{self.name}: {thing}")
+                self._recv(data.signed_integer)
+
+
+class AXIS_Driver(BusDriver):
+    def __init__(self, dut, name, clk, role="M"):
+        self._signals = [
+            "axis_tvalid",
+            "axis_tready",
+            "axis_tlast",
+            "axis_tdata",
+            "axis_tstrb",
+        ]
+        BusDriver.__init__(self, dut, name, clk)
+        self.clock = clk
+        self.dut = dut
+
+
+class M_AXIS_Driver(AXIS_Driver):
+    def __init__(self, dut, name, clk):
+        super().__init__(dut, name, clk)
+        self.bus.axis_tdata.value = 0
+        self.bus.axis_tstrb.value = 0xF
+        self.bus.axis_tlast.value = 0
+        self.bus.axis_tvalid.value = 0
+
+        self.rising_edge = RisingEdge(self.clock)
+        self.falling_edge = FallingEdge(self.clock)
+        self.read_only = ReadOnly()
+
+    async def wait_for_transaction(self):
+        await self.rising_edge  # transaction happens here
+        await self.read_only  # make sure everything is stable
+        if not self.bus.axis_tready.value:
+            # wait for ready from subordinate
+            await RisingEdge(self.bus.axis_tready)
+            await self.rising_edge  # transaction happens here
+        await self.falling_edge  # deassert valid after transaction
+
+    async def _driver_send(self, value, sync=True):
+        if value.get("type") == "pause":
+            await self.falling_edge
+            self.bus.axis_tvalid.value = 0  # set to 0 and be done.
+            self.bus.axis_tlast.value = 0  # set to 0 and be done.
+            for i in range(value.get("duration", 1)):
+                await self.rising_edge
+
+        elif value.get("type") == "write_single":
+            contents = value.get("contents", {})
+            data = contents.get("data", 0)
+            last = contents.get("last", 0)
+            await self.falling_edge
+            self.bus.axis_tdata.value = data
+            self.bus.axis_tlast.value = last
+            self.bus.axis_tvalid.value = 1
+            await self.wait_for_transaction()
+            self.bus.axis_tvalid.value = 0
+            self.bus.axis_tlast.value = 0
+        elif value.get("type") == "write_burst":
+            contents = value.get("contents", {})
+            data = contents.get("data", [0])
+            await self.falling_edge
+            for i, value in enumerate(data):
+                self.bus.axis_tdata.value = int(value)
+                if i == (len(data) - 1):
+                    self.bus.axis_tlast.value = 1
+                else:
+                    self.bus.axis_tlast.value = 0
+                self.bus.axis_tvalid.value = 1
+                await self.wait_for_transaction()
+                self.bus.axis_tvalid.value = 0
+                self.bus.axis_tlast.value = 0
+        else:
+            raise ValueError(f"Unknown command {value}")
+
+
+class S_AXIS_Driver(BusDriver):
+    def __init__(self, dut, name, clk):
+        self._signals = [
+            "axis_tvalid",
+            "axis_tready",
+            "axis_tlast",
+            "axis_tdata",
+            "axis_tstrb",
+        ]
+        AXIS_Driver.__init__(self, dut, name, clk)
+        self.bus.axis_tready.value = 0
+
+        self.rising_edge = RisingEdge(self.clock)
+        self.falling_edge = FallingEdge(self.clock)
+        self.read_only = ReadOnly()
+
+    async def _driver_send(self, value, sync=True):
+        if value.get("type") == "pause":
+            await self.falling_edge
+            self.bus.axis_tready.value = 0  # set to 0 and be done.
+            for i in range(value.get("duration", 1)):
+                await self.rising_edge
+        elif value.get("type") == "read":
+            await self.falling_edge
+            self.bus.axis_tready.value = 1
+            # wait for `duration` number of read transactions
+
+            duration = value.get("duration", 1)
+            # await self.rising_edge
+            for i in range(duration):
+                await self.read_only
+                if not self.bus.axis_tvalid.value:
+                    await RisingEdge(self.bus.axis_tvalid)
+                    await self.rising_edge  # transaction happens here
+                if i != (duration - 1):
+                    await self.falling_edge
+            await self.rising_edge
+            self.bus.axis_tready.value = 0
+        else:
+            raise ValueError(f"Unknown command {value}")
