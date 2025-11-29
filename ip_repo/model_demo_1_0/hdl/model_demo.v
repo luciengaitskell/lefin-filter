672334ef980e896e2c07a72ff5a36a14b3c28537
Luc Gaitskell <me@lucg.xyz>
2025-11-29 14:07:08 -0500

    Add demo model to ip

--- ip_repo/model_demo_1_0/hdl/model_demo.v
+++ ip_repo/model_demo_1_0/hdl/model_demo.v
@@ -10,11 +10,11 @@
 
 
 		// Parameters of Axi Slave Bus Interface S00_AXIS
-		parameter integer C_S00_AXIS_TDATA_WIDTH	= 32
+		parameter integer C_S00_AXIS_TDATA_WIDTH	= 64
 	)
 	(
 		// Users to add ports here
-
+        output wire classification,
 		// User ports ends
 		// Do not modify the ports beyond this line
 
@@ -28,21 +28,23 @@
 		input wire  s00_axis_tlast,
 		input wire  s00_axis_tvalid
 	);
-// Instantiation of Axi Bus Interface S00_AXIS
-	model_demo_slave_stream_v1_0_S00_AXIS # ( 
-		.C_S_AXIS_TDATA_WIDTH(C_S00_AXIS_TDATA_WIDTH)
-	) model_demo_slave_stream_v1_0_S00_AXIS_inst (
-		.S_AXIS_ACLK(s00_axis_aclk),
-		.S_AXIS_ARESETN(s00_axis_aresetn),
-		.S_AXIS_TREADY(s00_axis_tready),
-		.S_AXIS_TDATA(s00_axis_tdata),
-		.S_AXIS_TSTRB(s00_axis_tstrb),
-		.S_AXIS_TLAST(s00_axis_tlast),
-		.S_AXIS_TVALID(s00_axis_tvalid)
-	);
-
 	// Add user logic here
-
+	
+    demo_model #(
+          .C_S00_AXIS_TDATA_WIDTH(C_S00_AXIS_TDATA_WIDTH),
+          .INPUT_BIT_WIDTH          (8)
+    ) model_impl (
+          .aclk           (aclk),
+          .aresetn        (aresetn),
+          .weights        (weights),
+          .s00_axis_tlast (s00_axis_tlast),
+          .s00_axis_tvalid(s00_axis_tvalid),
+          .s00_axis_tdata (s00_axis_tdata),
+          .s00_axis_tstrb (s00_axis_tstrb),
+          .s00_axis_tready(s00_axis_tready),
+          .classification  (classification)
+      );
+    
 	// User logic ends
 
 	endmodule
