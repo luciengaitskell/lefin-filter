c79a619a911ed4af2f049bd02c4d95c390a1ec59
Luc Gaitskell <me@lucg.xyz>
2025-12-04 13:09:09 -0500

    Add model interface for Ethernet Subsystem AXI4-Stream bus (#13)
    * Add initial model interface module impl
    
    * Add initial strb and keep impl in AXIS tb helper
    
    * Add monitor kwargs to AXIS testbench class
    
    * Extend transaction counter by one more cycle
    
    * Remove ReadOnly from axis monitor transaction wait
    
    * Add in progress model interface testbench
    
    * Remove outdated prints
    
    * Read data as unsigned in axis helper
    
    * Slice out only expected transactions
    
    * Use islice instead of list
    
    * Allow immediate successive axis read operations
    
    * Go back to updating axis on falling edge
    
    I think it's easier to interpret.
    
    * Simplify axis read with write region helper
    
    * Fix axis read waiting to recheck valid input
    
    * Show data in read transaction print
    
    * Use log instead of print
    
    * Enable all test cases in tb
    
    * Randomly dont pause on writes in tb
    
    * Verify transaction count a second time after attempting extra reads
    
    * Add logic and testing for tlast propagation

---
+++ hdl/model/model_interface.sv
@@ -0,0 +1,63 @@
+
+module model_interface #(
+    parameter int C_S00_AXIS_TDATA_WIDTH = 32,
+    parameter int BIT_WIDTH = 8,
+    localparam int WIDTH = C_S00_AXIS_TDATA_WIDTH / BIT_WIDTH,
+    localparam int C_M00_AXIS_TDATA_WIDTH = C_S00_AXIS_TDATA_WIDTH,
+    parameter int MAXIMUM_INPUT_BYTES = 768
+) (
+    input wire aclk,
+    input wire aresetn,
+
+    // Ports of Axi Slave Bus Interface S00_AXIS
+    //  from the Ethernet Subsystem IP
+    input wire s00_axis_tlast,
+    s00_axis_tvalid,
+    input wire [C_S00_AXIS_TDATA_WIDTH-1 : 0] s00_axis_tdata,
+    input wire [WIDTH-1:0] s00_axis_tkeep,  // IP uses tkeep
+    output logic s00_axis_tready,
+
+    // Ports of Axi Master Bus Interface M00_AXIS
+    input wire m00_axis_tready,
+    output logic m00_axis_tvalid,
+    m00_axis_tlast,
+    output logic [C_M00_AXIS_TDATA_WIDTH-1 : 0] m00_axis_tdata,
+    output logic [WIDTH-1:0] m00_axis_tstrb
+    // we use tstrb in the pipeline as invalid bytes are still retain positional meaning
+);
+
+  localparam int MAXIMUM_CYCLES = MAXIMUM_INPUT_BYTES / WIDTH;
+  initial begin
+    assert (MAXIMUM_CYCLES * WIDTH == MAXIMUM_INPUT_BYTES)
+    else
+      $error(
+          "Error: MAXIMUM_INPUT_BYTES (%0d) must be a multiple of WIDTH (%0d)",
+          MAXIMUM_INPUT_BYTES,
+          WIDTH
+      );
+  end
+
+  wire s00_axis_transacted = s00_axis_tvalid && s00_axis_tready;
+
+  logic [$clog2(MAXIMUM_CYCLES+1)-1:0] s00_axis_transaction_count;
+  wire waiting_for_s00_axis_finish = (s00_axis_transaction_count == ($clog2(
+      MAXIMUM_CYCLES + 1
+  ))'(MAXIMUM_CYCLES));
+  counter #(
+      .MAXIMUM  (MAXIMUM_CYCLES+1),
+      .ROLL_OVER(0)
+  ) s00_axis_transaction_counter (
+      .clk    (aclk),
+      .rst    (!aresetn || (s00_axis_tlast && s00_axis_transacted)),
+      .trigger(s00_axis_transacted && !waiting_for_s00_axis_finish),
+      .count  (s00_axis_transaction_count)
+  );
+
+  assign s00_axis_tready = m00_axis_tready || waiting_for_s00_axis_finish;
+  assign m00_axis_tvalid = s00_axis_tvalid && !waiting_for_s00_axis_finish;
+  assign m00_axis_tdata  = s00_axis_tdata;
+  assign m00_axis_tstrb  = s00_axis_tkeep;
+  assign m00_axis_tlast = s00_axis_tlast || (s00_axis_transaction_count == ($clog2(
+      MAXIMUM_CYCLES + 1
+  ))'(MAXIMUM_CYCLES-1));  // one before going into waiting state
+endmodule
