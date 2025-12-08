1a7524a223782d90db59e72d0472006790ed0b5e
Luc Gaitskell <me@lucg.xyz>
2025-12-07 23:31:20 -0500

    Go back to using model classification output

--- bd/design_1/hdl/design_1_wrapper.v
+++ bd/design_1/hdl/design_1_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2025.1 (lin64) Build 6140274 Wed May 21 22:58:25 MDT 2025
-//Date        : Sun Dec  7 22:55:20 2025
+//Date        : Sun Dec  7 23:03:55 2025
 //Host        : grater running 64-bit Ubuntu 25.04
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
